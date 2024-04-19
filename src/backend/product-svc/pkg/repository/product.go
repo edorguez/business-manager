918e84b19c619b4c97cc7a1c6eaf7e88755a2096
edorguez <edro998@gmail.com>
2024-04-19 15:44:43 -0400

    Docker compose for mongodb and product svc and product repo

--- src/backend/product-svc/pkg/repository/product.go
+++ src/backend/product-svc/pkg/repository/product.go
@@ -1,13 +1,38 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"github.com/EdoRguez/business-manager/product-svc/pkg/config"
+	"github.com/EdoRguez/business-manager/product-svc/pkg/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type ProductRepo struct {
 	client *mongo.Client
+	config *config.Config
 }
+
+var collectionName = "products"
 
 func NewProductRepo(client *mongo.Client) *ProductRepo {
 	return &ProductRepo{
 		client: client,
 	}
+}
+
+func (productRepo *ProductRepo) CreateProduct(ctx context.Context, arg models.Product) (*models.Product, error) {
+	collection := productRepo.client.Database(productRepo.config.DBName).Collection(collectionName)
+
+	res, err := collection.InsertOne(ctx, arg)
+
+	if err != mongo.ErrNilCursor {
+		return nil, err
+	}
+
+	if _, ok := res.InsertedID.(primitive.ObjectID); ok {
+		return &arg, nil
+	}
+	return &arg, err
 }
