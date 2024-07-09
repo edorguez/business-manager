28fbc788e486042449fb568839f2a87bd0308752
edorguez <edro998@gmail.com>
2024-07-08 23:12:18 -0400

    Start login page

---
+++ src/ui/app/login/LoginClient.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const LoginClient = () => {
+  return (
+    <>
+      <h1>hola</h1>
+    </>
+  )
+}
+
+export default LoginClient;
---
+++ src/ui/app/login/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="grid grid-cols-8 h-screen">
+        <div className="col-span-3 bg-rose-200">
+          aa
+        </div>
+        <div className="col-span-5 bg-slate-200">
+          bb
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+export default LoginPage;
