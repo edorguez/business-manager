10b1b12bd7f22f990cdb76a82b7b5c69f924f38b
edorguez <edro998@gmail.com>
2025-01-20 23:04:36 -0400

    Create NextJS image and improve SEO

--- src/ui/app/layout.tsx
+++ src/ui/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Edezco",
   icons: {
     icon: "/images/favicon.ico",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -19,10 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Edezco</title>
+        <meta
+          name="description"
+          content="Crea tu tienda online y lleva control de todos tus productos y ventas en cualquier lugar"
+        />
+        <meta property="og:title" content="Edezco" />
+        <meta
+          property="og:description"
+          content="Crea tu tienda online y lleva control de todos tus productos y ventas en cualquier lugar"
+        />
+        <meta property="og:image" content="https://example.com/thumbnail.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </head>
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
